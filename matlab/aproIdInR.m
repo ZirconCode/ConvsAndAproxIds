847b600411759e1130cca8d7477c549da7e76944
Simon Grüning <sigrue@tl6.math.uzh.ch>
2016-12-30 20:36:28 +0100

    code

--- matlab/aproIdInR.m
+++ matlab/aproIdInR.m
@@ -1,5 +1,11 @@
+% Arbitrary Approximate Identity on R
+% First Example is the Poisson Kernel
+% Author: Simon Gruening
 
+figure('position',[0,0,400,400])
+hold on;
 
+% ---
 n = 1;
 k = @(x) (pi .* (x.^2 + 1)).^-1; % poisson
 %k = @(x) sqrt(1-x.^2)./pi; % half circle
@@ -12,5 +18,7 @@ hold on;
 for ep = 1:1:10;
     plot(I, ke(I,ep));
 end
+% ---
 
-
+set(gcf,'PaperPositionMode','auto','PaperSize',[5,5])
+saveas(gcf,'poissonkernel.pdf')
--- matlab/fejerkernel.m
+++ matlab/fejerkernel.m
@@ -1,19 +1,16 @@
+% The Fejer Kernel
+% Author: Simon Gruening
 
 figure('position',[0,0,400,400])
 hold on;
 
 % ---
-
 F = @(t,N) (1./(1+N)).*((sin(pi.*(N+1).*t)./sin(pi.*t))).^2;
-
-
 I = -0.5:0.001:0.5;
-
 
 for n=1:7
     plot(I,F(I,n));
 end
-
 % ---
 
 set(gcf,'PaperPositionMode','auto','PaperSize',[5,5])
